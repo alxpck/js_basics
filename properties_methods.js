230ba62b6fdd5a47d3f06de31adc82ac38e5efd5
Alex Peck <alex@alxpck.com>
2017-08-02 21:42:24 -0400

    madlibs

---
+++ properties_methods.js
@@ -0,0 +1,20 @@
+var passphrase = "Open Sesame";
+console.log(passphrase.length);
+
+// ......... OBJECT   . PROPERTY
+// .........passphrase is the object
+// .........length is it's property
+
+// MDN is a good place to learn more about JS
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+// ... OBJECT . METHOD()
+
+passphrase.toLowerCase()
+// ... OBJECT . METHOD()
+console.log(passphrase)
+// passphrase remains sentence case
+
+passphrase = passphrase.toLowerCase()
+console.log(passphrase)
+// passphrase is set to lowercase
