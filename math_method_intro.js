8404c40a2c692dcf7d521f0c59d23e5ac551102e
Alex Peck <alex@alxpck.com>
2017-08-03 21:41:53 -0400

    learning about Math library methods

---
+++ math_method_intro.js
@@ -0,0 +1,7 @@
+var testing;
+
+testing = Math.round(4.5353);
+
+console.log(testing);
+
+// more at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
--- scripts.js
+++
@@ -1,5 +0,0 @@
- var HTMLBadges = prompt('How many HTML badges do you have?');
- var CSSBadges = prompt('How many CSS badges do you have?');
- var totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
- // how do I check parseInt and parseFloat to make sure both are covered? 
- alert('Wow! You have ' + totalBadges + ' badges!');
