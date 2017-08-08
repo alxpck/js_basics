ee73fac7b64906f71f578b57ac38929bed99ae36
Alex Peck <alex@alxpck.com>
2017-08-07 21:29:34 -0400

    update while loop

--- while_loop.js
+++ while_loop.js
@@ -3,7 +3,7 @@ function randomNumber(upper) {
 }
 
 var counter = 0;
-while (counter < 10000) {
+while (counter < 100) {
 	var randNum = randomNumber(6);
 	document.write(randNum + ' ');
 	counter += 1;
